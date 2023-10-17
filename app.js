9d7c893b37cb6bd5b7e05a4d4e031687d9d7a607
Prasoon21 <prashun.parashar_cs19@gla.ac.in>
2023-10-17 22:57:07 +0530

    added request and responses

--- app.js
+++ app.js
@@ -1,7 +1,35 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log('Prashun Parashar');
+    const url = req.url;
+    if(url === '/home'){
+        res.write('<html>');
+        res.write('<head><title>Home</title></head>');
+        res.write('<body><h1>Welcome home</h1></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    else if(url === '/about'){
+        res.write('<html>');
+        res.write('<head><title>About Us</title></head>');
+        res.write('<body><h1>Welcome to About Us page</h1></body>');
+        res.write('</html>');
+        return res.end();
+
+    }
+    else if(url === '/node'){
+        res.write('<html>');
+        res.write('<head><title>My Node js</title></head>');
+        res.write('<body><h1>Welcome to my Node.js Project</h1></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(4000);
