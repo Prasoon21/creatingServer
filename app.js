17af68fcd032982837e822d3731cfe5c834e985c
Prasoon21 <prashun.parashar_cs19@gla.ac.in>
2023-10-18 08:57:17 +0530

    redirects to 302 response

--- app.js
+++ app.js
@@ -1,28 +1,34 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     const url = req.url;
-    if(url === '/home'){
+    const method = req.method;
+    if(url === '/'){
         res.write('<html>');
-        res.write('<head><title>Home</title></head>');
-        res.write('<body><h1>Welcome home</h1></body>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
         res.write('</html>');
         return res.end();
     }
-    else if(url === '/about'){
-        res.write('<html>');
-        res.write('<head><title>About Us</title></head>');
-        res.write('<body><h1>Welcome to About Us page</h1></body>');
-        res.write('</html>');
-        return res.end();
-
-    }
-    else if(url === '/node'){
-        res.write('<html>');
-        res.write('<head><title>My Node js</title></head>');
-        res.write('<body><h1>Welcome to my Node.js Project</h1></body>');
-        res.write('</html>');
-        return res.end();
+    if(url === '/message' && method === 'POST'){
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFile('message.txt', message, (err) => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
+            
+        });
+      //  fs.writeFileSync('message.txt', 'Dummy');
+       
     }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
